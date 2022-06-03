d2d801ea464b861a25ac92993eb1a8f37b7a1830
Konstantina <tinasto97@gmail.com>
2022-06-04 00:31:55 +0300

    add data to gauge from dataset

--- scripts/script.js
+++ scripts/script.js
@@ -4,141 +4,153 @@ data = {
   Pacific: { sustainable: 52, overexploited: 48 },
 };
 
-d3.csv("data/fish_stocks_within_sustainable_levels.csv", function (d) {
-  console.log(d);
-});
-
-var gaugeOptions = {
-  chart: {
-    type: "solidgauge",
+// process data
+d3.csv(
+  // "https://raw.githubusercontent.com/KonstantinaStoikou/dataviz/main/data/fish-stocks-within-sustainable-levels.csv?token=GHSAT0AAAAAABOUNHA6GE7YSMTJRUY5QGUIYU2Q6PQ",
+  "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-10-12/fish-stocks-within-sustainable-levels.csv",
+  function (d) {
+    return {
+      location: d.Entity,
+      year: d.Year,
+      overexploited: parseInt(d["Share of fish stocks that are overexploited"]),
+      sustainable: parseInt(
+        d[
+          "Share of fish stocks within biologically sustainable levels (FAO, 2020)"
+        ]
+      ),
+    };
   },
-
-  title: null,
-
-  pane: {
-    center: ["50%", "85%"],
-    size: "140%",
-    startAngle: -90,
-    endAngle: 90,
-    background: {
-      backgroundColor:
-        Highcharts.defaultOptions.legend.backgroundColor || "#EEE",
-      innerRadius: "60%",
-      outerRadius: "100%",
-      shape: "arc",
-    },
-  },
-
-  exporting: {
-    enabled: false,
-  },
-
-  tooltip: {
-    enabled: false,
-  },
-
-  // the value axis
-  yAxis: {
-    stops: [
-      [0.1, "#55BF3B"], // green
-      [0.5, "#DDDF0D"], // yellow
-      [0.9, "#DF5353"], // red
-    ],
-    lineWidth: 0,
-    tickWidth: 0,
-    minorTickInterval: null,
-    tickAmount: 2,
-    title: {
-      y: -70,
-    },
-    labels: {
-      y: 16,
-    },
-  },
-
-  plotOptions: {
-    solidgauge: {
-      dataLabels: {
-        y: 5,
-        borderWidth: 0,
-        useHTML: true,
+  function (data1) {
+    console.log("D", data1);
+    var gaugeOptions = {
+      chart: {
+        type: "solidgauge",
       },
-    },
-  },
-};
 
-// The speed gauge
-var chartSpeed = Highcharts.chart(
-  "container-speed",
-  Highcharts.merge(gaugeOptions, {
-    yAxis: {
-      min: 0,
-      max: 100,
-      title: {
-        text: "Overexploited share of fish stocks",
+      title: null,
+
+      pane: {
+        center: ["50%", "85%"],
+        size: "140%",
+        startAngle: -90,
+        endAngle: 90,
+        background: {
+          backgroundColor:
+            Highcharts.defaultOptions.legend.backgroundColor || "#EEE",
+          innerRadius: "60%",
+          outerRadius: "100%",
+          shape: "arc",
+        },
       },
-    },
 
-    credits: {
-      enabled: false,
-    },
+      exporting: {
+        enabled: false,
+      },
 
-    series: [
-      {
-        name: "Speed",
-        data: [
-          {
-            y: data.Atlantic.overexploited,
-            sust: data.Atlantic.sustainable,
-          },
+      tooltip: {
+        enabled: false,
+      },
+
+      // the value axis
+      yAxis: {
+        stops: [
+          [0.1, "#55BF3B"], // green
+          [0.5, "#DDDF0D"], // yellow
+          [0.9, "#DF5353"], // red
         ],
-        dataLabels: {
-          formatter: function () {
-            console.log(this);
-            return (
-              '<div style="text-align:center">' +
-              '<span style="font-size:25px">' +
-              this.y +
-              "%</span><br/>" +
-              '<span style="font-size:12px;opacity:0.4">' +
-              this.point.options.sust +
-              "% sustainable</span>" +
-              "</div>"
-            );
-          },
-          // format:
-          //   '<div style="text-align:center">' +
-          //   '<span style="font-size:25px">{y}%</span><br/>' +
-          //   '<span style="font-size:12px;opacity:0.4">{this.sust}% sustainable</span>' +
-          //   "</div>",
+        lineWidth: 0,
+        tickWidth: 0,
+        minorTickInterval: null,
+        tickAmount: 2,
+        title: {
+          y: -70,
         },
-        tooltip: {
-          valueSuffix: "%",
+        labels: {
+          y: 16,
         },
       },
-    ],
-  })
-);
 
-var activities = document.getElementById("country");
-
-activities.addEventListener("change", function () {
-  console.log(activities.value, data[activities.value].overexploited);
-
-  var point, newVal;
-
-  if (chartSpeed) {
-    point = chartSpeed.series[0].points[0];
-    console.log(chartSpeed.series);
-    newVal = {
-      y: data[activities.value].overexploited,
-      sust: data[activities.value].sustainable,
+      plotOptions: {
+        solidgauge: {
+          dataLabels: {
+            y: 5,
+            borderWidth: 0,
+            useHTML: true,
+          },
+        },
+      },
     };
 
-    point.update(newVal);
-  }
-});
+    // The speed gauge
+    var chartSpeed = Highcharts.chart(
+      "container-speed",
+      Highcharts.merge(gaugeOptions, {
+        yAxis: {
+          min: 0,
+          max: 100,
+          title: {
+            text: "Overexploited share of fish stocks",
+          },
+        },
 
-function find_percent(x) {
-  return 100 - x;
-}
+        credits: {
+          enabled: false,
+        },
+
+        series: [
+          {
+            name: "Speed",
+            data: [
+              {
+                y: data.Atlantic.overexploited,
+                sust: data.Atlantic.sustainable,
+              },
+            ],
+            dataLabels: {
+              formatter: function () {
+                console.log(this);
+                return (
+                  '<div style="text-align:center">' +
+                  '<span style="font-size:25px">' +
+                  this.y +
+                  "%</span><br/>" +
+                  '<span style="font-size:12px;opacity:0.4">' +
+                  this.point.options.sust +
+                  "% sustainable</span>" +
+                  "</div>"
+                );
+              },
+            },
+            tooltip: {
+              valueSuffix: "%",
+            },
+          },
+        ],
+      })
+    );
+
+    var activities = document.getElementById("region");
+
+    activities.addEventListener("change", function () {
+      var point, newVal;
+
+      if (chartSpeed) {
+        point = chartSpeed.series[0].points[0];
+        // newVal = {
+        //   y: data[activities.value].overexploited,
+        //   sust: data[activities.value].sustainable,
+        // };
+        datum = data1.filter(
+          (x) => x.location === activities.value && x.year === "2015"
+        )[0];
+        console.log(datum);
+        newVal = {
+          y: datum.overexploited,
+          sust: datum.sustainable,
+        };
+
+        point.update(newVal);
+      }
+    });
+  }
+);
